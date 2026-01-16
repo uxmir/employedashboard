61786184b9ef2ae637294a78f9585fed36923c7d
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-01-16 23:16:51 +0600

    employe dashboard started

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from './components/Container'
+import Employe from './components/EmployeDashboard/Employe'
+
+const App:React.FC = () => {
+  return (
+    <div>
+      <Employe/>
+    </div>
+  )
+}
+
+export default App
